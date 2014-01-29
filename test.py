3938664314b18c8ba41ff1f54e03dc1ae2ab781f
Thomas Anderson <driminary@gmail.com>
2014-01-29 20:12:30 +0000

    Temporary QR Code File
    Changed lines 24-27 in temp.py to send the QR code generated to a file
    'f.png' so that the image can then be passed through the file to line 35
    where it can be decoded.
    
    Program was successful in encoding and decoding the string "Hello".

--- test.py
+++ test.py
@@ -19,8 +19,11 @@ import qrcode
 import zbar
 import Image
 
-# image to import
+# create qr code
 f = qrcode.make("Hello")
+
+# save image
+f.save("f.png")
 
 # create a reader
 scanner = zbar.ImageScanner()
@@ -29,7 +32,7 @@ scanner = zbar.ImageScanner()
 scanner.parse_config('enable')
 
 # obtain image data
-pil = Image.open(f.convert('L'))
+pil = Image.open("f.png").convert('L')
 width, height = pil.size
 raw = pil.tostring()
 
@@ -42,7 +45,7 @@ scanner.scan(image)
 # extract results
 for symbol in image:
     # do something useful with results
-    print 'decoded', symbol.type, 'symbol', '"%s"' % symbol.data
+    print 'Image was a ', symbol.type, ', and contained the following message: \n', symbol.data
 
 # clean up
 del(image)
